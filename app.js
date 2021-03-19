e5878f7e7873b10f8ad9dc1a91f0619aaf1d24aa
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-19 11:27:58 -0700

    set up engine to use handlebars and changed extname to use 'hbs'. simple connection for now

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ const SequelizeStore = require("connect-session-sequelize");
 const mysql = require("mysql2");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
+
+// Error message to be used
+const errm = `ERR: ${err.message} ${err.stack}`;
+
+// Port
+const PORT = process.env.PORT || 3001;
+
+// engine settings
+app.set("view engine", "hbs");
+app.engine(
+    "hbs",
+    hbs({
+        extname: "hbs",
+        defaultLayout: "main",
+    })
+);
+
+// start server, will sequelize after working things work
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(errm);
+    }
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ db/blogtech_db.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE blogtech_db;
