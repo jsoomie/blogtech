987626a434e1d35e35c2094003e1bc1ecde7aac3
Jonathan Soukaseume <sksmejn@gmail.com>
2021-04-03 21:11:21 -0700

    ready for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        dialectOptions: { decimalNumbers: true },
-        logging: false,
-    }
-);
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(
+          process.env.DB_NAME,
+          process.env.DB_USER,
+          process.env.DB_PASS,
+          {
+              host: process.env.DB_HOST,
+              dialect: "mysql",
+              dialectOptions: { decimalNumbers: true },
+              logging: false,
+          }
+      );
 
 module.exports = sequelize;
